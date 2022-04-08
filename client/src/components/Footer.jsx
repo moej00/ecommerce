d7a22c22954651079507669c413086545466763e
moej00 <mj__89@hotmail.com>
2022-04-08 11:40:06 +0300

    ProductListPage

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>PayForLess</Logo>
+        <Description>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </Description>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <FacebookIcon />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <InstagramIcon />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <TwitterIcon />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accesories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Whishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <LocationOnIcon style={{ marginRight: "10px" }} />
+          1524, Orizaba Ave, Long Beach, CA
+        </ContactItem>
+        <ContactItem>
+          <ContactPhoneIcon style={{ marginRight: "10px" }} />
+          +1 562 347 7450
+        </ContactItem>
+        <ContactItem>
+          <MailOutlineIcon style={{ marginRight: "10px" }} />
+          contact@payforless.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
