1dd3adbd5d942bd905d80b59aa584468a38367d1
moej00 <67549705+moej00@users.noreply.github.com>
2022-04-22 12:01:38 +0300

    First commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
@@ -7,7 +13,27 @@ import Register from "./pages/Register";
 import Cart from "./pages/Cart";
 
 function App() {
-  return <Home />;
+  const user = false;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate replace to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={user ? <Navigate replace to={"/"} /> : <Register />}
+        />
+        <Route path="/product/:id" element={<Product />} />
+
+        <Route path="/prducts/category" element={<ProductList />} />
+
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
